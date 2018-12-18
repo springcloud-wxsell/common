c03a7e257febffb9ff7c9804b179a98254ea279f
wangwei <wang_wei608@163.com>
2018-12-18 21:20:09 +0800

    增加读写cookie的工具类和cookie常量，redis常量接口

---
+++ src/main/java/com/sell/common/constant/CookieConstant.java
@@ -0,0 +1,26 @@
+package com.sell.common.constant;
+
+/**
+ * @author WangWei
+ * @Title: CookieConstant
+ * @ProjectName common
+ * @date 2018/12/18 15:31
+ * @description:
+ */
+public interface CookieConstant {
+
+    /**
+     * 卖家用token
+     */
+    String TOKEN = "token";
+
+    /**
+     * 买家用openid
+     */
+    String OPENID = "openid";
+
+    /**
+     * 过期时间(单位: s)
+     */
+    Integer expire = 7200;
+}
---
+++ src/main/java/com/sell/common/constant/RedisConstant.java
@@ -0,0 +1,13 @@
+package com.sell.common.constant;
+
+/**
+ * @author WangWei
+ * @Title: RedisConstant
+ * @ProjectName common
+ * @date 2018/12/18 16:02
+ * @description:
+ */
+public interface RedisConstant {
+
+    String TOKEN_TEMPLATE = "token_%s";
+}
